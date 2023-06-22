3d19a85822954f8054b0e8dbf52a7787d27971fb
mickiyas123 <mikikinfe345@gmail.com>
2023-06-22 16:27:12 +0300

    feat: add prisma module

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -2,8 +2,8 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Global()
-@Module({})
-export class PrismaModule {
-  providers: [PrismaService];
-  exports: [PrismaService];
-}
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
