a3c088dd05a74b98eae8fc6cf64c798af0a445f3
mickiyas123 <mikikinfe345@gmail.com>
2023-06-27 14:47:30 +0300

    feat: run migration

--- prisma/migrations/20230627114540_init/migration.sql
+++ prisma/migrations/20230627114540_init/migration.sql
@@ -2,7 +2,10 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
+    "Password" TEXT,
+    "refresh_token" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
