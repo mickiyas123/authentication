a3c450360121c7f1986f8e78666531c7f624fae1
mickiyas123 <mikikinfe345@gmail.com>
2023-07-03 12:25:58 +0300

    fix: change to small letter

---
+++ prisma/migrations/20230630110035_change_spelling/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `Password` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "Password",
+ADD COLUMN     "password" TEXT;
