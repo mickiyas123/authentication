ed73ff0e1a2df000ee6645a7b948eb8e28439e05
mickiyas123 <mikikinfe345@gmail.com>
2023-07-03 12:27:38 +0300

    feat: configure different modules

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from './exceptions/prisma-client-exception.filter';
+import { CustomExceptionFilter } from './exceptions/custom-exception.filter';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 3600000 },
+    }),
+  );
+  app.useGlobalFilters(
+    new CustomExceptionFilter(),
+    new PrismaClientExceptionFilter(httpAdapter),
+  );
   app.useGlobalPipes(new ValidationPipe());
+  app.use(passport.initialize());
+  app.use(passport.session());
   await app.listen(3000);
 }
 bootstrap();
