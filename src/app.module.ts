87edc51fca36e3262a57607719154787616231b7
mickiyas123 <mikikinfe345@gmail.com>
2023-07-10 08:13:57 +0300

    feat: import created module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PermissionModule } from './permission/permission.module';
+import { RoleModule } from './role/role.module';
+import { AbilityModule } from './ability/ability.module';
 
 @Module({
   imports: [
@@ -14,6 +17,9 @@ import { UserModule } from './user/user.module';
     PrismaModule,
     AuthModule,
     UserModule,
+    PermissionModule,
+    RoleModule,
+    AbilityModule,
   ],
   controllers: [AppController],
   providers: [AppService],
